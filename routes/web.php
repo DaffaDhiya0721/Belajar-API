594cb3bcbd035039f7a8e5124cd8e6c8f01b0139
DaffaDhiya0721 <daffadhiya0721@gmail.com>
2024-08-02 14:47:33 +0700

    ubah tampilan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facedes\Routes;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
